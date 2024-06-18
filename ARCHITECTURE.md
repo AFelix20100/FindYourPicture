c0fb6adab3d2e0dc5214c344ab8cacd9001b0dac
ARTHUR Félix <arthur.felix160@gmail.com>
2024-06-19 00:30:25 +0200

    base + configuration bootstrap

---
+++ ARCHITECTURE.md
@@ -0,0 +1,9 @@
+Ce projet est un site web permettant de télécharger et de visualiser des images de façon
+aléatoire.
+
+On doit pouvoir saisir la résolution voulu et avoir des images selon les dimensions donnée.
+
+Liste des composants : 
+- app
+- navbar
+- album
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FindYourPicture';
+  ngOnInit() {
+    AOS.init();
+  }
 }
